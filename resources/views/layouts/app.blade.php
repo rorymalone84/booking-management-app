e1c7c33f9b7ea82af97df2bbd50ebddf5a0b8838
rory <rorymalone@live.com>
2024-02-26 13:20:19 +0000

    Admin layout created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,82 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <!-- Styles -->
+    @livewireStyles
+</head>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+<body class="min-h-screen font-sans antialiased bg-base-200/50 dark:bg-base-200">
+    {{-- NAVBAR mobile only --}}
+    <x-mary-nav sticky class="lg:hidden">
+        <x-slot:brand class="flex gap-2 items-center">
+            <x-mary-icon name="o-square-3-stack-3d" class="text-primary" />
+            <div>App</div>
+        </x-slot:brand>
+        <x-slot:actions>
+            <label for="main-drawer" class="lg:hidden mr-3">
+                <x-mary-icon name="o-bars-3" class="cursor-pointer" />
+            </label>
+        </x-slot:actions>
+    </x-mary-nav>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+    {{-- MAIN --}}
+    <x-mary-main full-width>
+        {{-- SIDEBAR --}}
+        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
 
-        @stack('modals')
+            {{-- BRAND --}}
+            <div class="p-6 pt-3 flex gap-3 items-center h-20">
+                <x-mary-icon name="o-square-3-stack-3d" class="text-primary" />
+                <div class="hidden-when-collapsed">App</div>
+            </div>
 
-        @livewireScripts
-    </body>
+            {{-- MENU --}}
+            <x-mary-menu activate-by-route>
+
+                {{-- User --}}
+                @if ($user = auth()->user())
+                    <x-mary-list-item :item="$user" sub-value="username" no-separator no-hover
+                        class="!-mx-2 mt-2 mb-5 border-y border-y-sky-900">
+                        <x-slot:actions>
+                            <x-mary-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" />
+                        </x-slot:actions>
+                    </x-mary-list-item>
+                @endif
+
+                <x-mary-menu-item title="Home" icon="o-home" link="/dashboard" />
+                <x-mary-menu-sub title="Users" icon="o-cog-6-tooth">
+                    <x-mary-menu-item title="Wifi" icon="o-wifi" link="####" />
+                    <x-mary-menu-item title="Archives" icon="o-archive-box" link="####" />
+                </x-mary-menu-sub>
+            </x-mary-menu>
+        </x-slot:sidebar>
+
+        {{-- The `$slot` goes here --}}
+        <x-slot:content>
+            {{ $slot }}
+        </x-slot:content>
+    </x-mary-main>
+
+    {{--  TOAST area --}}
+    <x-mary-toast />
+
+    @stack('modals')
+
+    @livewireScripts
+</body>
+
 </html>
